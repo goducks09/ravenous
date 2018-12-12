448cf54a1b3993c991d9ef4e090bf5542c60ae34
Chris <33887464+goducks09@users.noreply.github.com>
2018-12-12 14:25:37 -0800

    Add React component files and CSS files

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import './Business.css';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -37,3 +37,5 @@ class Business extends React.Component {
     )
   }
 }
+
+export default Business;
